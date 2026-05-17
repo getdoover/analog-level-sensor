9a31f05e56f0c014d1f616eb11a3047efa8bb9cb
mathsman5133 <mathsman5132@gmail.com>
2026-05-17 14:46:44 +1000

    add live tags

--- src/analog_level_sensor/app_tags.py
+++ src/analog_level_sensor/app_tags.py
@@ -2,7 +2,7 @@ from pydoover.tags import Tag, Tags
 
 
 class AnalogLevelSensorTags(Tags):
-    level_filled_percentage = Tag("number", default=None)
-    level_reading = Tag("number", default=None)
+    level_filled_percentage = Tag("number", default=None, live=True)
+    level_reading = Tag("number", default=None, live=True)
     raw_level_reading = Tag("number", default=None)
     level_volume = Tag("number", default=None)
